2029ad1ab57f7b2a315cfb4a8863418a758796c1
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-12-16 22:07:56 +0000

    Add get_profile_picture_url method and fix cloud name in view

--- neverforgotten/settings.py
+++ neverforgotten/settings.py
@@ -20,14 +20,22 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
+# ========================
+# Database Configuration
+# ========================
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+
 
 # ========================
 # Application Definition
