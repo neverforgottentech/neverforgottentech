e41affabd74e512acf9adbcbbb7a4830bcf47f29
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-12-16 21:23:16 +0000

    settings updated

--- neverforgotten/settings.py
+++ neverforgotten/settings.py
@@ -21,6 +21,14 @@ ALLOWED_HOSTS = [
 ]
 
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
 # ========================
 # Application Definition
 # ========================
