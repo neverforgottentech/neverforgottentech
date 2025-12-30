859c94a5f7dcb70ff391865b4269e97842aaed99
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-12-30 19:55:19 +0000

    delete memorial cloudinary foler

--- memorial/apps.py
+++ memorial/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MemorialConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'memorial'
+
+    def ready(self):
+        import memorial.signals  # This loads the signals
